c69c62f47d6bcb5a9a7a8ff456072ba2afb42f9f
Anjali Arora <anjalisharma888999@gmail.com>
2025-07-24 12:28:41 +0530

    Update RestaurantDetails.vue

--- src/components/RestaurantDetails.vue
+++ src/components/RestaurantDetails.vue
@@ -21,7 +21,8 @@
                 height="30"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="3"
+                stroke-width="1.5"
+                fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
               >
@@ -34,7 +35,8 @@
                 height="30"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="3"
+                stroke-width="1.5"
+                fill="none"
                 stroke-linecap="round"
                 stroke-linejoin="round"
               >
